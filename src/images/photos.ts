49eb5a9fbe9c067895cc47e511a4c573e681eb7a
Marcus Allen <marcus.allen84@thehutgroup.com>
2024-07-12 23:21:13 +0100

    added more photos and refactored code a fair bit

--- src/images/photos.ts
+++ src/images/photos.ts
@@ -1,16 +1,25 @@
 export interface Photos {
-  urls: string[];
+  url: string;
+  name: string;
 }
+export const filmPhotosLondon: Photos[] = [
+  { url: "https://i.postimg.cc/8cqp2wQY/R1-09930-007-A.jpg", name: "London" },
+  { url: "https://i.postimg.cc/rybwRy09/R1-09930-010-A.jpg", name: "Cologne" },
+  { url: "https://i.postimg.cc/2SNjTHxb/R1-09930-021-A.jpg", name: "Cologne" },
+  { url: "https://i.postimg.cc/MGDGnyVc/R1-09930-030-A.jpg", name: "London" },
+  { url: "https://i.postimg.cc/T1y2NFSm/R1-09930-003-A.jpg", name: "Cologne" },
+];
 
-export const icelandPhotos: Photos = {
-  urls: [
-    "https://i.postimg.cc/wvQ4wknR/DSF1384.jpg",
-
-    "https://i.postimg.cc/L6xK9mCg/DSF1351.jpg",
-    "https://i.postimg.cc/CKDQq0NN/DSF1382.jpg",
-
-    "https://i.postimg.cc/1tG2LSVt/DSF1418.jpg",
-    "https://i.postimg.cc/gcMNrrpp/DSF1389.jpg",
-    "https://i.postimg.cc/7Pgqk8rS/DSF1504.jpg",
-  ],
-};
+export const allPhotos: Photos[] = [
+  { url: "https://i.postimg.cc/wvQ4wknR/DSF1384.jpg", name: "Iceland" },
+  { url: "https://i.postimg.cc/L6xK9mCg/DSF1351.jpg", name: "Iceland" },
+  { url: "https://i.postimg.cc/CKDQq0NN/DSF1382.jpg", name: "Iceland" },
+  { url: "https://i.postimg.cc/1tG2LSVt/DSF1418.jpg", name: "Iceland" },
+  { url: "https://i.postimg.cc/gcMNrrpp/DSF1389.jpg", name: "Iceland" },
+  { url: "https://i.postimg.cc/7Pgqk8rS/DSF1504.jpg", name: "Iceland" },
+  { url: "https://i.postimg.cc/8cqp2wQY/R1-09930-007-A.jpg", name: "London" },
+  { url: "https://i.postimg.cc/rybwRy09/R1-09930-010-A.jpg", name: "Cologne" },
+  { url: "https://i.postimg.cc/2SNjTHxb/R1-09930-021-A.jpg", name: "Cologne" },
+  { url: "https://i.postimg.cc/MGDGnyVc/R1-09930-030-A.jpg", name: "London" },
+  { url: "https://i.postimg.cc/T1y2NFSm/R1-09930-003-A.jpg", name: "Cologne" },
+];
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const places = ["Iceland", "London", "Cologne"];
+const routes = places.map((place) => ({
+  path: place,
+  element: <App linkName={place} places={places} />,
+}));
+
+const router = createBrowserRouter([
+  ...routes,
+  {
+    path: "/",
+    element: <App linkName="" places={places} />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
