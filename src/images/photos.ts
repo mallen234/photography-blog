f1e09c00d80a6d063da8d22b864be76e520d5957
Marcus Allen <marcus.allen84@thehutgroup.com>
2024-08-29 18:04:20 +0100

    minor tweaks to images

--- src/images/photos.ts
+++ src/images/photos.ts
@@ -10,8 +10,48 @@ const imgNums = [
   1418, 1475, 1478, 1488, 1491, 1495, 1496, 1504, 1506,
 ];
 
+const penxtaxImgNums = [
+  "03",
+  "04",
+  "05",
+  "06",
+  "07",
+  "08",
+  "09",
+  "10",
+  "11",
+  "12",
+  "15",
+  "16",
+  "17",
+  "18",
+  "19",
+  "20",
+  "21",
+  "22",
+  "23",
+  "24",
+  "25",
+  "26",
+  "27",
+  "28",
+  "29",
+  "30",
+  "31",
+];
+
+const londonPhotos: Photos[] = penxtaxImgNums.map((penxtaxImgNum) => {
+  return {
+    url: `https://marcus-photo-bucket.s3.eu-west-2.amazonaws.com/R1-09930-0${penxtaxImgNum}A.JPG`,
+    name: "London",
+  };
+});
+
+// const pentaxPhotos = "https://marcus-photo-bucket.s3.eu-west-2.amazonaws.com/R1-09930-0"
+// const suffix =
+
 export const icelandPhotos = imgNums.map((img) => {
-  return { url: urlPrefix.concat(img.toString(), urlSuffix), name: "London" };
+  return { url: urlPrefix.concat(img.toString(), urlSuffix), name: "Iceland" };
 });
 
 export const filmPhotosLondon: Photos[] = [
@@ -22,4 +62,4 @@ export const filmPhotosLondon: Photos[] = [
   { url: `${urlPrefix}`, name: "Cologne" },
 ];
 
-export const allPhotos: Photos[] = icelandPhotos;
+export const allPhotos: Photos[] = icelandPhotos.concat(londonPhotos);
